9adc584dbcfbdffd8a529d703584a1e0bb15e55c
CrazyBoyFeng <CrazyBoyFeng@Live.Com>
2021-08-24 11:46:00 +0800

    Extend Command.kt

--- app/src/main/kotlin/com/github/crazyboyfeng/accSettings/acc/Command.kt
+++ app/src/main/kotlin/com/github/crazyboyfeng/accSettings/acc/Command.kt
@@ -3,14 +3,7 @@ package com.github.crazyboyfeng.accSettings.acc
 import com.topjohnwu.superuser.Shell
 
 object Command {
-    private fun exec(vararg options: String): String {
-        val command = buildString {
-            append("/dev/.vr25/acc/acca")
-            for (option in options) {
-                append(" --")
-                append(option)
-            }
-        }
+    fun exec(command: String): String {
         val result = Shell.su(command).exec()
         if (result.isSuccess) {
             return result.out.joinToString("\n").trim()
@@ -32,8 +25,25 @@ object Command {
         }
     }
 
-    fun set(property:String,vararg values:String)=exec(property,values.joinToString(" "))
-    fun getVersion(): String = exec("version")
+    private fun execAcc(vararg options: String): String {
+        val command = buildString {
+            append("/dev/.vr25/acc/acca")
+            for (option in options) {
+                append(" --")
+                append(option)
+            }
+        }
+        return exec(command)
+    }
+
+    fun setConfig(property: String, vararg values: String?) =
+        execAcc("set \"$property=${values.joinToString(" ")}\"")
+
+    fun getConfig(property: String = "") = execAcc("set", "print $property")
+
+    fun getDefaultConfig(property: String = "") = execAcc("set", "print-default $property")
+
+    fun getVersion(): String = execAcc("version")
     open class AccException : Exception {
         constructor()
         constructor(message: String) : super(message)
