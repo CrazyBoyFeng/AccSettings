0842f5b4cde1a870b340a37551a3f1701525fe4a
CrazyBoyFeng <CrazyBoyFeng@Live.Com>
2021-08-25 16:02:58 +0800

    Attach `AccDataStore`

--- app/src/main/kotlin/com/github/crazyboyfeng/accSettings/fragment/SettingsFragment.kt
+++ app/src/main/kotlin/com/github/crazyboyfeng/accSettings/fragment/SettingsFragment.kt
@@ -3,9 +3,11 @@ package com.github.crazyboyfeng.accSettings.fragment
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
 import com.github.crazyboyfeng.accSettings.R
+import com.github.crazyboyfeng.accSettings.data.AccDataStore
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings_preferences, rootKey)
+        preferenceManager.preferenceDataStore = AccDataStore(requireContext())
     }
 }
