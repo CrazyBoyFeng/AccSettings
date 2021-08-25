37c9894c875ac6261568ec6d2f6575ca75edcc06
CrazyBoyFeng <CrazyBoyFeng@Live.Com>
2021-08-25 15:59:01 +0800

    Handle `daemon`

--- app/src/main/kotlin/com/github/crazyboyfeng/accSettings/data/AccDataStore.kt
+++ app/src/main/kotlin/com/github/crazyboyfeng/accSettings/data/AccDataStore.kt
@@ -3,19 +3,16 @@ package com.github.crazyboyfeng.accSettings.data
 import android.content.Context
 import androidx.preference.PreferenceDataStore
 import com.github.crazyboyfeng.accSettings.R
+import com.github.crazyboyfeng.accSettings.acc.Command
 
-class AccDataStore(private val context: Context):PreferenceDataStore() {
-    override fun getBoolean(key: String?, defValue: Boolean): Boolean {
-        if (key==context.getString(R.string.daemon)){
-            //todo if daemon
-        }
-        return super.getBoolean(key, defValue)
+class AccDataStore(private val context: Context) : PreferenceDataStore() {
+    override fun getBoolean(key: String?, defValue: Boolean): Boolean = when (key) {
+        context.getString(R.string.acc_daemon) -> Command.isDaemonRunning()
+        else -> super.getBoolean(key, defValue)
     }
 
-    override fun putBoolean(key: String?, value: Boolean) {
-        if (key==context.getString(R.string.daemon)){
-            //todo if daemon
-        }
-        super.putBoolean(key, value)
+    override fun putBoolean(key: String?, value: Boolean) = when (key) {
+        context.getString(R.string.acc_daemon) -> Command.setDaemonRunning(value)
+        else -> super.putBoolean(key, value)
     }
 }
