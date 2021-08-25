c1565816c9bbb60e0fea6b74e3fa15f674dd57c8
CrazyBoyFeng <CrazyBoyFeng@Live.Com>
2021-08-25 12:18:29 +0800

    Refresh subtitle with fragment

--- app/src/main/kotlin/com/github/crazyboyfeng/accSettings/SettingsActivity.kt
+++ app/src/main/kotlin/com/github/crazyboyfeng/accSettings/SettingsActivity.kt
@@ -2,11 +2,18 @@ package com.github.crazyboyfeng.accSettings
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+import com.github.crazyboyfeng.accSettings.fragment.SettingsFragment
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportFragmentManager.addOnBackStackChangedListener {
+            val preferenceFragment =
+                supportFragmentManager.findFragmentById(android.R.id.content) as PreferenceFragmentCompat
+            supportActionBar?.subtitle = preferenceFragment.preferenceScreen.title
+        }
         supportFragmentManager
             .beginTransaction()
             .replace(android.R.id.content, SettingsFragment())
