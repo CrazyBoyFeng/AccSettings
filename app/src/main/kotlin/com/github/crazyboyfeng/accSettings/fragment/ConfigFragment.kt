69a961796874ee10f8e6cc53d96b98cc24472aba
CrazyBoyFeng <CrazyBoyFeng@Live.Com>
2021-08-25 12:19:30 +0800

    Initial ConfigFragment.kt

--- app/src/main/kotlin/com/github/crazyboyfeng/accSettings/fragment/ConfigFragment.kt
+++ app/src/main/kotlin/com/github/crazyboyfeng/accSettings/fragment/ConfigFragment.kt
@@ -8,7 +8,6 @@ import com.github.crazyboyfeng.accSettings.data.ConfigDataStore
 class ConfigFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.config_preferences, rootKey)
-        requireActivity().title=preferenceScreen.title//todo change on activity
         preferenceManager.preferenceDataStore = ConfigDataStore()
     }
 }
