e97c0052fd96f67a0b00fc72aee1f3cac5ffb971
CrazyBoyFeng <CrazyBoyFeng@Live.Com>
2021-08-27 01:38:36 +0800

    Update dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
 //    implementation("com.google.android.material:material:1.4.0")
 //    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
     implementation("androidx.preference:preference-ktx:1.1.1")
-    implementation("com.github.topjohnwu.libsu:core:3.0.2")
+    implementation("com.github.topjohnwu.libsu:core:3.1.2")
 //    testImplementation("junit:junit:4.13.2")
 //    androidTestImplementation("androidx.test.ext:junit:1.1.3")
 //    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
