f14f7981d7adc630ba81b098a90f1b7e7343fd7a
CrazyBoyFeng <CrazyBoyFeng@Live.Com>
2021-08-29 01:25:55 +0800

    Update denpendency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     compileSdk = 30
     defaultConfig {
         applicationId = "com.github.crazyboyfeng.accSettings"
-        minSdk = 14
+        minSdk = 16
         targetSdk = 30
         versionCode = 20210819
         versionName = "2021.8.19"
@@ -40,7 +40,7 @@ dependencies {
 //    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
     implementation("androidx.preference:preference-ktx:1.1.1")
     implementation("com.github.topjohnwu.libsu:core:3.1.2")
-    implementation("com.github.invissvenska:NumberPickerPreference:1.0.3")
+    implementation("com.github.CrazyBoyFeng.AndroidXPreferenceExtensions:edittext:0.4.2")
 //    testImplementation("junit:junit:4.13.2")
 //    androidTestImplementation("androidx.test.ext:junit:1.1.3")
 //    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
