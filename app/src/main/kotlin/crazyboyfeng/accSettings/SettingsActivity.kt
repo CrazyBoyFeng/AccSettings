e400a68d8f437c4fe657e4716c2e89ce8d51ead8
CrazyBoyFeng <CrazyBoyFeng@Live.Com>
2021-08-29 13:10:16 +0800

    Fix root checking

--- app/src/main/kotlin/crazyboyfeng/accSettings/SettingsActivity.kt
+++ app/src/main/kotlin/crazyboyfeng/accSettings/SettingsActivity.kt
@@ -1,8 +1,10 @@
 package crazyboyfeng.accSettings
 
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.ContentFrameLayout
 import androidx.preference.PreferenceFragmentCompat
 import crazyboyfeng.accSettings.acc.Command
 import crazyboyfeng.accSettings.fragment.SettingsFragment
@@ -10,9 +12,12 @@ import crazyboyfeng.accSettings.fragment.SettingsFragment
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if(!Command.isRoot()){
-            val textView=findViewById<TextView>(android.R.id.content)
+        if (!Command.isRoot()) {
+            val textView = TextView(this)
             textView.setText(R.string.need_root_permission)
+            textView.gravity = Gravity.CENTER
+            val contentFrameLayout = findViewById<ContentFrameLayout>(android.R.id.content)
+            contentFrameLayout.addView(textView)
             return
         }
         supportFragmentManager.addOnBackStackChangedListener {
