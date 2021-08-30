9a26792b3b54aa5a41434a9ae6790640141a993d
CrazyBoyFeng <CrazyBoyFeng@Live.Com>
2021-08-31 01:17:29 +0800

    Register boot and update receiver

---
+++ app/src/main/kotlin/crazyboyfeng/accSettings/DaemonReceiver.kt
@@ -0,0 +1,11 @@
+package crazyboyfeng.accSettings
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class DaemonReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("Not yet implemented")
+    }
+}
