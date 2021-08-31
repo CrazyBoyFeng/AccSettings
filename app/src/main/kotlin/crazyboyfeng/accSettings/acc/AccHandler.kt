041b0c44ae8cf4e78fa3ac443d15890655d3e1fb
CrazyBoyFeng <CrazyBoyFeng@Live.Com>
2021-08-31 17:10:10 +0800

    Implement `install` and `update`

---
+++ app/src/main/kotlin/crazyboyfeng/accSettings/acc/AccHandler.kt
@@ -0,0 +1,53 @@
+package crazyboyfeng.accSettings.acc
+
+import android.content.Context
+import crazyboyfeng.accSettings.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+
+
+class AccHandler {
+    //todo Should I throw an exception or return the result?
+    //todo notice to read log when failed
+    private suspend fun install(context: Context) {
+        suspend fun cacheAssertFile(fileName: String): File = withContext(Dispatchers.IO) {
+            val cachedFile = File(context.cacheDir, fileName)
+            context.assets.open(fileName).use { input ->
+                FileOutputStream(cachedFile).use { output ->
+                    input.copyTo(output)
+                }
+            }
+            cachedFile
+        }
+
+        val resources = context.resources
+        val accVersionCode = resources.getInteger(R.integer.acc_version_code)
+        val accVersionName = resources.getString(R.string.acc_version_name)
+        try {
+            cacheAssertFile("acc_v${accVersionName}_.${accVersionCode}.tar.gz")
+            val installShFile = cacheAssertFile("install-tarball.sh")
+            val command = "sh ${installShFile.absolutePath} acc --non-interactive"
+            Command.exec(command)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun start() {
+
+    }
+
+    @Throws(Command.AccException::class)
+    suspend fun update(context: Context) {
+        val installedVersionCode = Command.getVersion().first
+        val bundledVersionCode = context.resources.getInteger(R.integer.acc_version_code)
+        if (bundledVersionCode <= installedVersionCode) {
+            return
+        }
+        install(context)
+        start()
+    }
+}
