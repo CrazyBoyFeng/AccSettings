797bc7a4a97b289b647d4a5ca19ee0a5411abe5e
CrazyBoyFeng <CrazyBoyFeng@Live.Com>
2021-09-08 23:53:08 +0800

    Clean unused comments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,10 +34,6 @@ android {
 
 dependencies {
 //    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-//    implementation("androidx.core:core-ktx:1.6.0")
-//    implementation("androidx.appcompat:appcompat:1.3.1")
-//    implementation("com.google.android.material:material:1.4.0")
-//    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
     implementation("androidx.preference:preference-ktx:1.1.1")
     implementation("androidx.work:work-runtime:2.6.0")
     implementation("com.github.topjohnwu.libsu:core:3.1.2")
