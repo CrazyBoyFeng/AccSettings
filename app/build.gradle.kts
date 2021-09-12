f9343c14858c2d6d72d75f270ea88efbaf20d5ee
CrazyBoyFeng <CrazyBoyFeng@Live.Com>
2021-09-12 23:39:46 +0800

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "crazyboyfeng.accSettings"
         minSdk = 14
         targetSdk = 30
-        versionCode = 202109090
-        versionName = "2021.9.9"
+        versionCode = 202109120
+        versionName = "2021.9.12"
 //        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
         resValue("string", "version_name", versionName!!)
     }
