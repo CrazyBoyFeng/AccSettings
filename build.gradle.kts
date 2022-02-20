2eee6c3969088ba260f8f39eed0cec625e4bef89
CrazyBoyFeng <CrazyBoyFeng@Live.Com>
2022-02-20 15:45:53 +0800

    Upgrade AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
+plugins {
+    id("com.android.application").version("7.1.1").apply(false)
+    id("org.jetbrains.kotlin.android").version("1.6.10").apply(false)
 }
 
-allprojects {
-    repositories {
-        maven {
-            setUrl("https://jitpack.io")
-        }
-    }
-}
-
-tasks.wrapper{
+tasks.wrapper {
     enabled = false
 }
